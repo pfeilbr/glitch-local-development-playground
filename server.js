0088fa417e25139e6504a3618329087cbd61a881
Glitch (just-narwhal) <none>
2019-02-18 18:23:03 +0000

    🌖👨‍👨‍👦 Checkpoint ./server.js:57484/785

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path')
+
 const { execSync } = require('child_process')
 const app = express();
 
 const rp = require('request-promise');
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+const localBranchName = 'local';
 
-// http://expressjs.com/en/starter/static-files.html
+
+app.use(bodyParser.json())
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
+
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
@@ -23,30 +26,31 @@ app.get('/status', (req, resp) => {
   resp.send({"msg": "all good"});
 })
 
-app.post('/deploy', (request, response) => {
-  if (request.query.secret !== process.env.SECRET) {
-    response.status(401).send()
-    return
-  }
+app.get('/deploy', (request, response) => {
+//   if (request.query.secret !== process.env.SECRET) {
+//     response.status(401).send()
+//     return
+//   }
 
-  if (request.body.ref !== 'refs/heads/glitch') {
-    response
-      .status(200)
-      .send('Push was not to glitch branch, so did not deploy.')
-    return
-  }
+//   if (request.body.ref !== 'refs/heads/glitch') {
+//     response
+//       .status(200)
+//       .send('Push was not to glitch branch, so did not deploy.')
+//     return
+//   }
 
-  const repoUrl = request.body.repository.git_url
+//   const repoUrl = request.body.repository.git_url
 
-  console.log('Fetching latest changes.')
+//   console.log('Fetching latest changes.')
   const output = execSync(
-    `git checkout -- ./ && git pull -X theirs ${repoUrl} glitch && refresh`
+    `git merge ${localBranchName} && refresh`
   ).toString()
-  console.log(output)
-  response.status(200).send()
+  console.log(output);
+  response.send({output});
 })
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
+  console.log(process.env);
 });
