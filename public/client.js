d05d6fc3e8a4f2fd303147cfa1d9ceb0bead4e33
Brian Pfeil <brian.pfeil@gmail.com>
2019-02-18 13:50:57 -0500

    local changes

--- public/client.js
+++ public/client.js
@@ -1,7 +1,7 @@
 // client-side js
 // run by the browser each time your view template is loaded
 
-console.log('hello world :o');
+console.log('hello worlds');
 
 // our default array of dreams
 const dreams = [
