997169a427e68065490b45342b34af990b527f19
Brian Pfeil <brian.pfeil@gmail.com>
2019-02-19 19:51:58 -0500

    local changes

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
+# glitch-local-development-playground
+
+Develop [Glitch](https://glitch.com) apps locally.  Based on [noise-machines/git-glitched](https://github.com/noise-machines/git-glitched).
+The default Glitch development experience is to use the browser based editor.  This allows you to use your favorite local editor.
+
+## Resources
+
+* [Possible to code locally and push to glitch with git?](https://support.glitch.com/t/possible-to-code-locally-and-push-to-glitch-with-git/2704/3)
+* [Code locally, push to glitch via git?](https://support.glitch.com/t/code-locally-push-to-glitch-via-git/4227/5?u=tim)
+
 ### live reload from local changes
 
 ```sh
--- server.js
+++ server.js
@@ -1,56 +1,35 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-const path = require('path')
-
-const { execSync } = require('child_process')
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const { execSync } = require("child_process");
+const rp = require("request-promise");
 const app = express();
 
-const rp = require('request-promise');
+const localBranchName = "local";
 
-const localBranchName = 'local';
+app.use(bodyParser.json());
+app.use(express.static("public"));
 
-
-app.use(bodyParser.json())
-app.use(express.static('public'));
-
-
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + "/views/index.html");
 });
 
-app.get('/dbg', async (req, resp) => {
-  resp.send({"body": await rp('https://google.com')});
-})
+app.get("/dbg", async (req, resp) => {
+  resp.send({ body: await rp("https://google.com") });
+});
 
-app.get('/status', (req, resp) => {
-  resp.send({"msg": "all good"});
-})
+app.get("/status", (req, resp) => {
+  resp.send({ msg: "all good" });
+});
 
-app.get('/deploy', (request, response) => {
-//   if (request.query.secret !== process.env.SECRET) {
-//     response.status(401).send()
-//     return
-//   }
-
-//   if (request.body.ref !== 'refs/heads/glitch') {
-//     response
-//       .status(200)
-//       .send('Push was not to glitch branch, so did not deploy.')
-//     return
-//   }
-
-//   const repoUrl = request.body.repository.git_url
-
-//   console.log('Fetching latest changes.')
+app.get("/deploy", (request, response) => {
   const output = execSync(
     `git merge -X theirs ${localBranchName} && refresh`
-  ).toString()
+  ).toString();
   console.log(output);
-  response.send({output});
-})
+  response.send({ output });
+});
 
-
-// listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("app is listening on port " + listener.address().port);
 });
